b71566ffb8c6aea19504def75d502356a2dd97af
liushsh <liushsh@cn.ibm.com>
2017-03-01 10:01:50 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,32 @@
+# coding: utf-8
+
+# In[4]:
+
+import sys
 import os
-try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+import json
+sys.path.append(os.path.join(os.getcwd(),'..'))
+import watson_developer_cloud
+import watson_developer_cloud.natural_language_understanding.features.v1 as \
+    features
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+# In[3]:
+
+nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
+    version='2017-02-27',
+    username='YOUR SERVICE USERNAME',
+    password='YOUR SERVICE PASSWORD')
+
+
+# In[6]:
+
+response = nlu.analyze(
+    text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+         'Superman fears not Banner, but Wayne.',
+    features=[features.Entities(), features.Keywords()])
+
+print(json.dumps(response, indent=2))
+
+# In[ ]:
 
