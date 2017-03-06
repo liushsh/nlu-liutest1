af2db9b18cdb7eac45e284aa2a60726cbfe91feb
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2017-03-06 10:59:45 -0500

    fixed expample to be a flask server

--- server.py
+++ server.py
@@ -1,32 +1,30 @@
-# coding: utf-8
-
-# In[4]:
-
 import sys
 import os
 import json
+from flask import Flask
+from flask import jsonify
+
 sys.path.append(os.path.join(os.getcwd(),'..'))
 import watson_developer_cloud
 import watson_developer_cloud.natural_language_understanding.features.v1 as \
     features
 
 
-# In[3]:
+app = Flask("NLU App")
 
 nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
-    version='2017-02-27',
-    username='ddc3ed94-6032-417b-ac2e-ab7faa8ee11f',
-    password='4ct36lkd1p4Z')
+        version='2017-02-27',
+        username=os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_USERNAME'),
+        password=os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_PASSWORD'))
+
+@app.route("/")
+def eval_default():
+        response = nlu.analyze(
+                text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+                'Superman fears not Banner, but Wayne.',
+                features=[features.Entities(), features.Keywords()])
+        return jsonify(response)
 
 
-# In[6]:
-
-response = nlu.analyze(
-    text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
-         'Superman fears not Banner, but Wayne.',
-    features=[features.Entities(), features.Keywords()])
-
-print(json.dumps(response, indent=2))
-
-# In[ ]:
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True,port=int(os.getenv('PORT',8080)))
